5501d5592adc9a47c5705e36b79ad497d8e2d2b3
Vishal <nocapvishal@gmail.com>
2026-02-07 21:22:06 +0530

    render cursorglow

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${outfit.variable}`}>
+        <CursorGlow />
         {children}
       </body>
     </html>
